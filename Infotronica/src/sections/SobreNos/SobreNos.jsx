834999406da846d3bd86394f04aefe9051e1e0e1
Joao <joao.vod06@gmail.com>
2025-11-12 16:28:20 -0300

    Implementação da parte "SobreNos" v0.15

--- Infotronica/src/sections/SobreNos/SobreNos.jsx
+++ Infotronica/src/sections/SobreNos/SobreNos.jsx
@@ -1,19 +1,32 @@
-export default function SobreNos(){
-    return(
-        <>
-         <section id="secao-sobre-nos-nav" class="secao-pagina secao-sobre-nos">
-            <div class="logo-i-superior-direito">I</div>
-                <div class="wrapper-conteudo-sobre-nos">
-                    <div class="conteudo-texto">
-                        <h2 class="titulo-secao">SOBRE NÓS</h2>
-                            <p>Na Infotrônica, acreditamos que a tecnologia deve simplificar a vida. Desde 1997, entregamos soluções inovadoras para você, seu negócio e sua equipe.</p>
-                        <p>Nossa dedicação é impulsionada pela busca constante por excelência e inovação. Com produtos de alta qualidade e serviços especializados, garantimos que você tenha sempre o melhor suporte técnico e as soluções mais avançadas.</p>
-                    </div>
-                <div class="container-imagem">
-                    <img src="./img/pessoa a usar laptop.avif" alt="Pessoa a usar um laptop com luz ambiente roxa"/>
-                </div>
-            </div>
-        </section>
-        </>
-    )
+import styles from './SobreNos.module.css';
+import img from '../../../public/nootebook moderno.jpg';
+
+export default function SobreNos() {
+  return (
+    <section id="secao-sobre-nos-nav" className={`${styles.secaoPagina} ${styles.secaoSobreNos}`}>
+      <div className={styles.logoISuperiorDireito}>I</div>
+
+      <div className={styles.wrapperConteudoSobreNos}>
+        <div className={styles.conteudoTexto}>
+          <h2 className={styles.tituloSecao}>SOBRE NÓS</h2>
+          <p>
+            Na Infotrônica, acreditamos que a tecnologia deve simplificar a vida. Desde 1997, entregamos soluções
+            inovadoras para você, seu negócio e sua equipe.
+          </p>
+          <p>
+            Nossa dedicação é impulsionada pela busca constante por excelência e inovação. Com produtos de alta
+            qualidade e serviços especializados, garantimos que você tenha sempre o melhor suporte técnico e as
+            soluções mais avançadas.
+          </p>
+        </div>
+
+        <div className={styles.containerImagem}>
+          <img
+            src={img}
+            alt="Pessoa a usar um laptop com luz ambiente roxa"
+          />
+        </div>
+      </div>
+    </section>
+  );
 }
