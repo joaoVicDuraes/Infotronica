8670235ccf7a5a9ef23110b61a636f210046844f
Joao <joao.vod06@gmail.com>
2025-11-17 11:03:19 -0300

    v0.20 comecando a aplicar responsividade

--- Infotronica/src/components/layout/Header/Header.jsx
+++ Infotronica/src/components/layout/Header/Header.jsx
@@ -1,33 +1,53 @@
+import { useState } from 'react'
 import Logo from '../../../assets/logo2certo.png'
 import style from './Header.module.css'
 import Button from '../../common/Button/Button'
 import SectionTitle from '../../common/SectionTitle/SectionTitle'
 
 export default function Header() {
-    return(
+    const [menuAberto, setMenuAberto] = useState(false)
+
+    const toggleMenu = () => setMenuAberto(!menuAberto)
+
+    return (
         <>
-        
-        <div className='cabecalho'>
-            <header> 
-                <ul className='header'>
-                    <Button texto="INICIO" href="#inicio"/>
-                    <Button texto="SERVIÇOS" href="#servicos"/>
-                    <Button texto="COMPUTADORES" href="#computadores"/>
-                </ul>
-                <img src={Logo} alt="Logo da Infotrônica" className={style.logoDireita} />
-                <ul>
-                    <Button texto="PERIFÉRICOS" href="#perifericos"/>
-                    <Button texto="CONTATO" href="#footer"/>
-                    <a href='https://wa.me/5511971440699' target="_blank" rel="noopener noreferrer">
-                    <Button texto="ORÇAMENTO" />
-                    </a>
-                </ul>
-            </header>
-        </div>
+            <div className="cabecalho">
+                <header className={style.header}>
+
+                    {/* BOTÃO HAMBURGUER */}
+                    <div className={style.hamburguer} onClick={toggleMenu}>
+                        <span className={style.linha}></span>
+                        <span className={style.linha}></span>
+                        <span className={style.linha}></span>
+                    </div>
+
+                    {/* LOGO */}
+                    <img src={Logo} alt="Logo da Infotrônica" className={style.logoDireita} />
+
+                    {/* MENU DESKTOP / MOBILE COMBINADO */}
+                    <ul className={menuAberto ? style.menuMobileAberto : style.menuDesktop}>
+                        <Button texto="INICIO" href="#inicio" />
+                        <Button texto="SERVIÇOS" href="#servicos" />
+                        <Button texto="COMPUTADORES" href="#computadores" />
+                        <Button texto="PERIFÉRICOS" href="#perifericos" />
+                        <Button texto="CONTATO" href="#footer" />
+
+                        <a href='https://wa.me/5511971440699'
+                           target="_blank"
+                           rel="noopener noreferrer">
+                            <Button texto="ORÇAMENTO" />
+                        </a>
+                    </ul>
+
+                </header>
+            </div>
+
             <div className={style.conteudoPrincipal} id='inicio'>
                 <SectionTitle titulo="INFOTRÔNICA" />
-                <p className={style.subtituloHero} id='servicos'>INOVAÇÃO & TECNOLOGIA DESDE 1997</p>
+                <p className={style.subtituloHero} id='servicos'>
+                    INOVAÇÃO & TECNOLOGIA DESDE 1997
+                </p>
             </div>
         </>
-    ) 
+    )
 }
